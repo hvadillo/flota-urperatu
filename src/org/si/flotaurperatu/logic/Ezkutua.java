d01a499023c79931a9d18d55f815d88a68f8ce65
Andoni723 <andoni9623@gmail.com>
2017-03-04 17:48:44 +0100

    Ezkutua klasea sortuta

---
+++ src/org/si/flotaurperatu/logic/Ezkutua.java
@@ -0,0 +1,15 @@
+package org.si.flotaurperatu.logic;
+
+public class Ezkutua extends Arma{
+
+	public Ezkutua(String pIzena, int pKostua, int pKop) {
+		super(pIzena, pKostua, pKop);
+	}
+
+	@Override
+	public void erabili() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
