e901b37963e16446f245e727be1190b09322dbca
hvadillo <vadillo.hector@gmail.com>
2017-03-07 11:13:07 +0100

    ku6fy

--- src/org/si/flotaurperatu/interf/Leihoa.java
+++ src/org/si/flotaurperatu/interf/Leihoa.java
@@ -33,5 +33,6 @@ public class Leihoa extends JFrame implements Observer{
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub 
+		//kjabfvñafjbvjfa
 	}
 }
