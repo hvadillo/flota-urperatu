73953049d6ce99a0d71f8c9d16927ab4b4a1ea67
Andoni723 <andoni9623@gmail.com>
2017-03-10 11:32:50 +0100

    Ezkutuaren kolpeak

--- src/org/si/flotaurperatu/logic/Ezkutua.java
+++ src/org/si/flotaurperatu/logic/Ezkutua.java
@@ -2,14 +2,25 @@ package org.si.flotaurperatu.logic;
 
 public class Ezkutua extends Arma{
 
+	private int kolpeak;
+	
 	public Ezkutua(int pKop) {
 		super("Ezkutua",5,pKop);
+		kolpeak = 2;
 	}
 
 	@Override
 	public void erabili() {
 		// TODO Auto-generated method stub
 		//
+	}
+	public void kolpeaJaso(Arma pArma){
+		if(pArma instanceof Bonba){
+			kolpeak--;
+		}
+		else{
+			kolpeak = 0;
+		}
 	}
 
 }
